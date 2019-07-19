7b8c0af47d587ee31c5fff3a23629328a05dcffe
zlp <1131808958@qq.com>
2019-07-19 14:53:22 +0800

    添加mode配置 适应日期、时间、日期时间选择

--- components/timePicker/index.js
+++ components/timePicker/index.js
@@ -4,12 +4,19 @@ Component({
      * 组件的属性列表
      */
     properties: {
-
+        /**
+         * 组件类型 date:日期(yyyy-MM-dd) time：时间(hh:mm:ss) dateTime: 日期时间(yyyy-MM-dd hh:mm:ss)
+         */
+        mode: {
+            type: String,
+            value: 'date',
+            observer: 'modeChange'
+        },
         disabled: {
             type: Boolean,
             value: false
         },
-        inputHolder: String
+        placeholder: String
     },
 
     /**
@@ -18,8 +25,6 @@ Component({
     data: {
         pickerArray: [], //日期控件数据list
         pickerIndex: [], //日期控件选择的index
-        chooseIndex: [], //日期控件确认选择的index
-        chooseArray: [], //日期控件确认选择后的list
         dateString: '' //页面显示日期
     },
 
@@ -27,87 +32,191 @@ Component({
      * 组件的方法列表
      */
     methods: {
+
+        modeChange: function(newVal, oldVal) {
+            this.getPickerArray(newVal)
+        },
         //补零
         formatNumber(n) {
             n = n.toString()
             return n[1] ? n : '0' + n
         },
-        _onInit() {
+        //日期时间格式化
+        formateDateTime(arr) {
+            let mode = this.data.mode
+            switch (mode) {
+                case 'date':
+                    return arr.map(this.formatNumber).join('-')
+                    break;
+                case 'time':
+                    return arr.map(this.formatNumber).join(':')
+                    break;
+
+                case 'dateTime':
+                    return arr.slice(0, 3).map(this.formatNumber).join('-') + ' ' + arr.slice(3, 6).map(this.formatNumber).join(':')
+                    break;
+            }
+        },
+
+        /**
+         *
+         * 获取本月天数
+         * @param {number} year
+         * @param {number} month
+         * @param {number} [day=0] 0为本月0最后一天的
+         * @returns number 1-31
+         */
+        _getNumOfDays(year, month, day = 0) {
+            return new Date(year, month, day).getDate();
+        },
+
+        //获取pickerArray
+        getPickerArray(mode = this.data.mode) {
             let date = new Date();
-            let pickerArray = this.data.pickerArray;
-            //时
+            let pickerArray = []
+                //年
+            let year = [];
+            for (let i = date.getFullYear() - 5; i <= date.getFullYear() + 5; i++) {
+                year.push({ id: i, name: i + '年' });
+            }
+            let currentYear = date.getFullYear();
+            let yearIndex = year.findIndex(item => item.id == currentYear);
+            pickerArray.push({
+                    picker: 'year',
+                    value: year,
+                    pickerIndex: yearIndex
+                })
+                //月
+            let month = [];
+            for (let i = 1; i <= 12; i++) {
+                month.push({ id: i, name: i + '月' });
+            }
+            pickerArray.push({
+                    picker: 'month',
+                    value: month,
+                    pickerIndex: date.getMonth(),
+                })
+                //日
+            let dayNum = this._getNumOfDays(date.getFullYear(), date.getMonth() + 1);
+            let day = [];
+            for (let i = 1; i <= dayNum; i++) {
+                day.push({ id: i, name: i + '日' });
+            }
+            pickerArray.push({
+                    picker: 'day',
+                    value: day,
+                    pickerIndex: date.getDate() - 1,
+                })
+                //时
             let time = [];
             for (let i = 0; i <= 23; i++) {
                 time.push({ id: i, name: this.formatNumber(i) });
             }
-            //分
-            let division = [];
+            pickerArray.push({
+                    picker: 'time',
+                    value: time,
+                    pickerIndex: date.getHours(),
+                })
+                //分
+            let minutes = [];
             for (let i = 0; i <= 59; i++) {
-                division.push({ id: i, name: this.formatNumber(i) });
+                minutes.push({ id: i, name: this.formatNumber(i) });
             }
-            //秒
+            pickerArray.push({
+                    picker: 'minutes',
+                    value: minutes,
+                    pickerIndex: date.getMinutes(),
+                })
+                //秒
             let seconds = [];
             for (let i = 0; i <= 59; i++) {
                 seconds.push({ id: i, name: this.formatNumber(i) });
             }
+            pickerArray.push({
+                picker: 'seconds',
+                value: seconds,
+                pickerIndex: date.getSeconds(),
+            })
 
-            //选项
-            pickerArray[0] = time;
-            pickerArray[1] = division;
-            pickerArray[2] = seconds;
-            let detail = {
-                value: this.formatNumber(date.getHours()) +
-                    ':' +
-                    this.formatNumber(date.getMinutes()) +
-                    ':' +
-                    this.formatNumber(date.getSeconds())
-            };
+            let pickerIndex = []
+                //过滤不同mode的pickerArray keys
+            let formatPickerArray = () => {
+                    switch (mode) {
+                        case 'date':
+                            return ['year', 'month', 'day']
+                            break;
+                        case 'dateTime':
+                            return ['year', 'month', 'day', 'time', 'minutes', 'seconds']
+                            break;
+                        case 'time':
+                            return ['time', 'minutes', 'seconds']
+                            break;
+                    }
+                }
+                //过滤不同mode的pickerArray values
+            let pickerValues = formatPickerArray(mode)
+            let formatPickers = []
+                // pickerArray.filter(item =>
+                //     pickerValues.indexOf(item.picker) >= 0
+                // )
+                //获取pickers选项和默认选择下标
+            pickerArray.map(item => {
+                if (pickerValues.indexOf(item.picker) >= 0) {
+                    pickerIndex.push(item.pickerIndex)
+                    formatPickers.push(item.value)
+                }
+            })
             this.setData({
-                pickerArray,
-                pickerIndex: [
-                    date.getHours(),
-                    date.getMinutes(),
-                    date.getSeconds()
-                ],
-                dateString: detail.value
-            });
-            this.chooseIndex = [
-                date.getHours(),
-                date.getMinutes(),
-                date.getSeconds()
-            ];
-            this.data.chooseArray = pickerArray;
+                    pickerArray: formatPickers,
+                    pickerIndex
+                })
+                //通过下标获取对应时间
+            let currentDate = this.getPickerValue(pickerIndex)
+            this.setData({
+                dateString: currentDate
+            })
+
         },
 
-        pickerChange: function(e) {
-            let indexArr = e.detail.value;
-            const time = this.data.pickerArray[0][indexArr[0]].id;
-            const division = this.data.pickerArray[1][indexArr[1]].id;
-            const seconds = this.data.pickerArray[2][indexArr[2]].id;
-            let detail = {
-                value: this.formatNumber(time) +
-                    ':' +
-                    this.formatNumber(division) +
-                    ':' +
-                    this.formatNumber(seconds)
-            };
+        getPickerValue(pickerIndex) {
+            let date = this.data.pickerArray.map((item, index) =>
+                // console.log(this.data.pickerArray[index][pickerIndex[index]].id, pickerIndex)
+                this.data.pickerArray[index][pickerIndex[index]].id
+            )
+            let dateString = this.formateDateTime(date)
 
-            this.data.chooseIndex = e.detail.value;
-            this.data.chooseArray = this.data.pickerArray;
+            return dateString
+        },
+        pickerChange: function(e) {
+            let currentDate = this.getPickerValue(e.detail.value)
             this.setData({
-                dateString: detail.value
+                dateString: currentDate
             })
+            let detail = {
+                value: currentDate
+            }
             this.triggerEvent('onPickerChange', detail);
         },
+        //月变化时获取当月多少天
         pickerColumnChange: function(e) {
-
+            let value = e.detail
+            let year = this.data.pickerArray[0][this.data.pickerIndex[0]].id
+            if (this.data.mode !== 'time') {
+                if (value.column === 1) {
+                    let days = this._getNumOfDays(year, value.value + 1)
+                    let day = [];
+                    for (let i = 1; i <= days; i++) {
+                        day.push({ id: i, name: i + '日' });
+                    }
+                    this.data.pickerArray[2] = day
+                    this.setData({
+                        pickerArray: this.data.pickerArray
+                    })
+                }
+            }
         },
-        pickerCancel: function(e) {
-            this.setData({
-                pickerIndex: this.data.chooseIndex,
-                pickerArray: this.data.chooseArray
-            });
-        }
+        pickerCancel: function(e) {}
+
     },
 
     lifetimes: {
@@ -118,7 +227,7 @@ Component({
             // 在组件实例被从页面节点树移除时执行
         },
         ready() {
-            this._onInit();
+            this.getPickerArray()
         }
     }
 });
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,12 +4,20 @@ const app = getApp()
 
 Page({
     data: {
-
+        items: [{ name: '日期选择', value: 'date', checked: 'true' }, { name: '时间选择', value: 'time' }, { name: '日期时间选择', value: 'dateTime' }],
+        mode: 'date'
     },
     onPickerChange(e) {
 
         console.log("onPickerChange", e)
     },
+    radioChange: function(e) {
+        console.log(e)
+        this.setData({
+            mode: e.detail.value
+        })
+    },
+
     onLoad: function() {
 
     },
